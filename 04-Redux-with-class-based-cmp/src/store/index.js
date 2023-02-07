21dc9a136c946c8c479fd1382f0383fad3faeea9
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-07 08:47:30 +0530

    Added scripts

---
+++ 04-Redux-with-class-based-cmp/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if(action.type === 'increment') {
+        console.log('counter incremented in index.js');
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if(action.type === 'decrement') {
+        console.log('counter incremented in index.js');
+        return {
+            counter: state.counter - 1
+        };
+    }
+    return state;
+};
+
+// redux store
+const store = redux.createStore(counterReducer);
+
+export default store;
