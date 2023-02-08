d07a8a9863209cb2bf68805c7095549bafc1cdbc
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-08 07:38:40 +0530

    Added scripts

---
+++ 09-Reading-and-dispatching/src/App.js
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile'
+
+// tap store and read value from store
+import { useSelector } from 'react-redux';
+
+function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  
+  
+  return (
+    <Fragment>
+      <Header/>
+      {!isAuth && <Auth/>}
+      {isAuth && <UserProfile />}
+      <Counter/>
+    </Fragment>
+    
+  );
+}
+
+export default App;
