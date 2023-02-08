77e2c68e2295bffaaf0b3c5309833b2e83c112c5
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-08 07:36:33 +0530

    Added scripts

---
+++ 08-Working-with-multiple-slices/src/store/index.js
@@ -0,0 +1,87 @@
+// import { createStore } from 'redux';
+// configureStore like createStore creates a store but it makes merging multiple reducers
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+// const redux = require('redux');
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+    // preparing slice of a global state
+    name: 'counter',
+    initialState: initialCounterState,
+    reducers: {
+        // methods
+        increment(state) {
+            // allowed to mutate state
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        toogleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+
+const intialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: intialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
+// cS.reducer we can get access to reducers set up in slice and this can cause problrm in bigger application hence configure store used
+// const store = createStore(counterSlice.reducer);
+const store = configureStore({
+    // configure store will merge all reducers into one big reducer
+    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer}
+});
+
+// below will return obj full of key ex: cS.a.d() returns action obj of this shape, {type: 'some auto-generated unique identifier'}
+export const counterActions = counterSlice.actions;
+
+export const authActions = authSlice.actions;
+
+/*NOT NEEDED
+const counterReducer = (state = initialState, action) => {
+    // redux exchange existing state with brand new state, it doesn't merge states
+    if(action.type === 'increment') {
+        console.log('counter incremented in index.js');
+        return {
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        };
+    }
+    if(action.type === 'decrement') {
+        console.log('counter decremented in index.js');
+        return {
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        };
+    }
+    if(action.type === 'toggle') {
+        console.log('counter toggled in index.js');
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
+        };
+    }
+
+    return state;
+};
+
+// redux store
+const store = createStore(counterReducer);
+*/
+
+export default store;
